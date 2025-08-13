646af81c4ff732eedf32f17cc7c4a25e23598f4b
ThiagoROX <thiagokenis@live.com>
2025-08-13 06:37:50 -0300

    Refactor player regen event handling
    Simplified type checking by using pattern matching for Player instance and reordered condition checks for clarity. Replaced variable 'p' with 'player' for consistency and improved code readability.

--- src/main/java/kernitus/plugin/OldCombatMechanics/module/ModulePlayerRegen.java
+++ src/main/java/kernitus/plugin/OldCombatMechanics/module/ModulePlayerRegen.java
@@ -35,41 +35,43 @@ public class ModulePlayerRegen extends OCMModule {
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onRegen(final EntityRegainHealthEvent e) {
-        if (e.getEntityType() != EntityType.PLAYER
-                || e.getRegainReason() != EntityRegainHealthEvent.RegainReason.SATIATED)
+        if (!(e.getEntity() instanceof final Player player)) {
             return;
+        }
 
-        final Player p = (Player) e.getEntity();
+        if (e.getRegainReason() != EntityRegainHealthEvent.RegainReason.SATIATED) {
+            return;
+        }
 
-        if (!isEnabled(p.getWorld())) return;
-        final UUID playerId = p.getUniqueId();
+        if (!isEnabled(player.getWorld())) return;
+        final UUID playerId = player.getUniqueId();
 
         // We cancel the regen, but saturation and exhaustion need to be adjusted separately
         // Exhaustion is modified in the next tick, and saturation in the tick following that (if exhaustion > 4)
         e.setCancelled(true);
 
         // Get exhaustion & saturation values before healing modifies them
-        final float previousExhaustion = p.getExhaustion();
-        final float previousSaturation = p.getSaturation();
+        final float previousExhaustion = player.getExhaustion();
+        final float previousSaturation = player.getSaturation();
 
         // Check that it has been at least x seconds since last heal
         final long currentTime = System.currentTimeMillis();
         final boolean hasLastHealTime = healTimes.containsKey(playerId);
         final long lastHealTime = healTimes.computeIfAbsent(playerId, id -> currentTime);
 
-        debug("Exh: " + previousExhaustion + " Sat: " + previousSaturation + " Time: " + (currentTime - lastHealTime), p);
+        debug("Exh: " + previousExhaustion + " Sat: " + previousSaturation + " Time: " + (currentTime - lastHealTime), player);
 
         // If we're skipping this heal, we must fix the exhaustion in the following tick
         if (hasLastHealTime && currentTime - lastHealTime <= module().getLong("interval")) {
-            Bukkit.getScheduler().runTaskLater(plugin, () -> p.setExhaustion(previousExhaustion), 1L);
+            Bukkit.getScheduler().runTaskLater(plugin, () -> player.setExhaustion(previousExhaustion), 1L);
             return;
         }
 
-        final double maxHealth = Objects.requireNonNull(p.getAttribute(Attribute.MAX_HEALTH)).getValue();
-        final double playerHealth = p.getHealth();
+        final double maxHealth = Objects.requireNonNull(player.getAttribute(Attribute.MAX_HEALTH)).getValue();
+        final double playerHealth = player.getHealth();
 
         if (playerHealth < maxHealth) {
-            p.setHealth(MathsHelper.clamp(playerHealth + module().getInt("amount"), 0.0, maxHealth));
+            player.setHealth(MathsHelper.clamp(playerHealth + module().getInt("amount"), 0.0, maxHealth));
             healTimes.put(playerId, currentTime);
         }
 
@@ -78,9 +80,9 @@ public class ModulePlayerRegen extends OCMModule {
 
         Bukkit.getScheduler().runTaskLater(plugin, () -> {
             // We do this in the next tick because bukkit doesn't stop the exhaustion change when cancelling the event
-            p.setExhaustion(previousExhaustion + exhaustionToApply);
-            debug("Exh before: " + previousExhaustion + " Now: " + p.getExhaustion() +
-                    " Sat now: " + previousSaturation, p);
+            player.setExhaustion(previousExhaustion + exhaustionToApply);
+            debug("Exh before: " + previousExhaustion + " Now: " + player.getExhaustion() +
+                    " Sat now: " + previousSaturation, player);
         }, 1L);
     }
 
