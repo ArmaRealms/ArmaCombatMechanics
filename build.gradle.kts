a3329f2449d5d1f32252f3a12e37eb816f3486a3
ThiagoROX <thiagokenis@live.com>
2025-08-13 05:31:44 -0300

    Update build config and dependencies
    Switched to 'com.gradleup.shadow' plugin v9.0.1 and updated dependency versions for Spigot API and PlaceholderAPI. Changed Java toolchain to version 21 and improved shadowJar output naming. Removed unused repositories and dependencies for a cleaner build configuration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,8 @@
-/*
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/.
- */
-
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
     `java-library`
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "9.0.1"
 }
 
 repositories {
@@ -23,18 +17,14 @@ repositories {
     maven("https://repo.codemc.org/repository/maven-public/")
     // Netty library from Minecraft
     maven("https://libraries.minecraft.net/")
-    // Spartan API
-    maven("https://repo.crazycrew.us/api")
 }
 
 dependencies {
     implementation("org.bstats:bstats-bukkit:3.0.2")
-    compileOnly("org.spigotmc:spigot-api:1.20-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.21.8-R0.1-SNAPSHOT")
     // Shaded in by Bukkit
     compileOnly("io.netty:netty-all:4.1.93.Final")
-    compileOnly("me.clip:placeholderapi:2.11.3")
-    // Spartan API
-    compileOnly("me.vagdedes.spartan:SpartanAPI:9.0")
+    compileOnly("me.clip:placeholderapi:2.11.6")
 }
 
 group = "kernitus.plugin.OldCombatMechanics"
@@ -43,7 +33,7 @@ description = "OldCombatMechanics"
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(8))
+        languageVersion.set(JavaLanguageVersion.of(21))
     }
 }
 
@@ -72,7 +62,7 @@ tasks.named("assemble").configure {
 }
 
 tasks.named<ShadowJar>("shadowJar") {
-    archiveFileName.set("${project.name}.jar")
+    archiveFileName.set("${project.name}-${project.version}.jar")
     dependencies {
         relocate("org.bstats", "kernitus.plugin.OldCombatMechanics.lib.bstats")
     }
